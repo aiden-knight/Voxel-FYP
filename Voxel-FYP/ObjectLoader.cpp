42dfe7302557914d859a842e48a2dc355d4b3449
Aiden Knight <worgenaiden@gmail.com>
2025-01-25 16:11:47 +0000

    Added normal generation to obj loader

--- Voxel-FYP/ObjectLoader.cpp
+++ Voxel-FYP/ObjectLoader.cpp
@@ -4,6 +4,7 @@
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <tiny_obj_loader.h>
 
+#include <optional>
 
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/hash.hpp>
@@ -36,46 +37,71 @@ namespace ObjectLoader
         mesh.max = glm::vec3(std::numeric_limits<float>::min());
         std::unordered_map<Vertex, uint32_t> uniqueVertices{};
 
-        for (const auto& shape : shapes) {
-            for (const auto& index : shape.mesh.indices) {
-                Vertex vertex{};
+        if (shapes.size() > 1)
+            throw std::runtime_error("Require model to have triangles only");
 
-                vertex.pos = {
-                    attrib.vertices[3 * index.vertex_index + 0],
-                    attrib.vertices[3 * index.vertex_index + 1],
-                    attrib.vertices[3 * index.vertex_index + 2]
-                };
+        for (const auto& shape : shapes) 
+        {
+            for (size_t i = 0; i < shape.mesh.indices.size(); i+=3)
+            {
+                bool calculateNormal = true;
+                std::vector<Vertex> triangleVertices;
+                triangleVertices.reserve(3);
 
-                if (index.normal_index >= 0) {
-                    vertex.normal = {
-                        attrib.normals[3 * index.normal_index + 0],
-                        attrib.normals[3 * index.normal_index + 1],
-                        attrib.normals[3 * index.normal_index + 2]
+                // get vertices from the attrib
+                for (size_t j = 0; j < 3; ++j)
+                {
+                    const auto& index = shape.mesh.indices[i+j];
+                    Vertex vertex{};
+
+                    vertex.pos = {
+                        attrib.vertices[3 * index.vertex_index + 0],
+                        attrib.vertices[3 * index.vertex_index + 1],
+                        attrib.vertices[3 * index.vertex_index + 2]
                     };
-                }
-                else {
-                    vertex.normal = { 0.0f, 0.0f, 0.0f };
-                }
 
-                /*vertex.texCoord = {
-                    attrib.texcoords[2 * index.texcoord_index + 0],
-                    1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
-                };
-
-                vertex.color = { 1.0f, 1.0f, 1.0f };*/
-
-                if (uniqueVertices.count(vertex) == 0) {
-                    uniqueVertices[vertex] = static_cast<uint32_t>(mesh.vertices.size());
-                    for (int i = 0; i < 3; ++i)
-                    {
-                        mesh.max[i] = std::max(mesh.max[i], vertex.pos[i]);
-                        mesh.min[i] = std::min(mesh.min[i], vertex.pos[i]);
+                    if (index.normal_index >= 0) {
+                        vertex.normal = {
+                            attrib.normals[3 * index.normal_index + 0],
+                            attrib.normals[3 * index.normal_index + 1],
+                            attrib.normals[3 * index.normal_index + 2]
+                        };
+                        calculateNormal = false;
                     }
-                    mesh.vertices.push_back(vertex);
+                    triangleVertices.push_back(vertex);
                 }
 
-                mesh.indices.push_back(uniqueVertices[vertex]);
-            }
+
+                // calculate the normal of the vertices if needed
+                if (calculateNormal)
+                {
+                    glm::vec3 edge0 = triangleVertices[0].pos - triangleVertices[2].pos;
+                    glm::vec3 edge1 = triangleVertices[1].pos - triangleVertices[2].pos;
+                    glm::vec3 normal = glm::normalize(glm::cross(edge0, edge1));
+                    for (auto& vertex : triangleVertices)
+                    {
+                        vertex.normal = normal;
+                    }
+                }
+
+                // add the vertices and indices to the mesh
+                for (auto& vertex : triangleVertices)
+                {
+                    if (uniqueVertices.count(vertex) == 0)
+                    {
+                        uniqueVertices[vertex] = static_cast<uint32_t>(mesh.vertices.size());
+                        for (int j = 0; j < 3; ++j)
+                        {
+                            mesh.max[j] = std::max(mesh.max[j], vertex.pos[j]);
+                            mesh.min[j] = std::min(mesh.min[j], vertex.pos[j]);
+                        }
+
+                        mesh.vertices.push_back(vertex);
+                    }
+
+                    mesh.indices.push_back(uniqueVertices[vertex]);
+                }
+            }            
         }
         return mesh;
     }
